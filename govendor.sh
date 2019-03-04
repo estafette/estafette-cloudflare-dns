1efd0d8fdfba6ca095eec143b44edad4aa98bd5e
Jorrit Salverda <jsalverda@travix.com>
2019-03-04 15:16:36 +0100

    build with go modules

--- govendor.sh
+++
@@ -1,7 +0,0 @@
-govendor init
-govendor fetch github.com/ericchiang/k8s@v0.2.0
-govendor fetch github.com/prometheus/client_golang/prometheus@v0.8.0
-govendor fetch github.com/prometheus/client_golang/prometheus/promhttp@v0.8.0
-govendor fetch github.com/stretchr/testify/assert@v1.1.4
-govendor fetch github.com/stretchr/testify/mock@v1.1.4
-govendor fetch github.com/rs/zerolog/log@v1.2.2
